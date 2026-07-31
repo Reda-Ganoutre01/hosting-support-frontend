8933ba39e60246d86631a6576f4deb893ffcf78e
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-31 21:20:55 +0100

    feat: implement AuthService for authentication with axios

---
+++ src/services/AuthService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+class AuthService{
+  constructor (){
+    this.http=axios.create({baseURL: "/api/auth"});
+    
+  }
+  async authenticate(credentials){
+    return await  axios.http.post("/login",credentials);
+  }
+
+}
+
+
+export default new AuthService();
