1ad8907d02e0df4fb142edba3303766008c46ec1
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-08-07 18:03:22 +0100

    feat(main): integrate Redux Provider and App component in main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/index.css'
+import { Provider } from 'react-redux'
+import App from './app/App'
+import store from './lib/store'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   
+  <Provider store={store}>
+
+    <App/>
+  </Provider>
   </StrictMode>,
 )
