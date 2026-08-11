b01b5b4541bc69a354f8b350e27c85cbf3fa1213
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-08-11 23:48:30 +0100

    feat: initialize project structure with core components, utility helpers, and authentication pages

--- src/app/AppRouter.jsx
+++ src/app/AppRouter.jsx
@@ -1,23 +1,22 @@
 import AuthProvider from "@/context/AuthProvider.jsx";
 import {Suspense} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoadingPage from "../pages/loading/LoadingPage.jsx";
+import { LoadingPage } from "../pages/loading/LoadingPage.jsx";
 
+import HomePage from "../pages/HomePage.jsx";
+import LoginPage from "../pages/LoginPage.jsx";
 
 export function AppRouter (){
-
-
   return(
     <AuthProvider>
       <Suspense fallback={<LoadingPage />}>
         <Router>
           <Routes>
-              <Route path="/" element={<LoadingPage />} />
-
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<HomePage />} />
           </Routes>
         </Router>
       </Suspense>
-
     </AuthProvider>
   );
 }
