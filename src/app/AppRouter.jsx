f19d9a416f435e5c8a2151313c1f59856442da6d
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-08-16 13:38:55 +0100

    feat: initialize AppRouter with authentication context and route definitions

--- src/app/AppRouter.jsx
+++ src/app/AppRouter.jsx
@@ -1,18 +1,21 @@
 import AuthProvider from "@/context/AuthProvider.jsx";
-import {Suspense} from "react";
+import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoadingPage } from "../pages/loading/LoadingPage.jsx";
 
 import HomePage from "../pages/HomePage.jsx";
 import LoginPage from "../pages/LoginPage.jsx";
+import RegisterPage from "../pages/RegisterPage.jsx";
 
-export function AppRouter (){
-  return(
+export function AppRouter () {
+  return (
     <AuthProvider>
       <Suspense fallback={<LoadingPage />}>
         <Router>
           <Routes>
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/signup" element={<RegisterPage />} />
             <Route path="/" element={<HomePage />} />
           </Routes>
         </Router>
